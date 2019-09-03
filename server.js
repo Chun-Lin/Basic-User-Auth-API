9ec1a708c82f0f5d21db509b2c0d80be4b44a51b
Chun-Lin <wulin40063@gmail.com>
2019-09-03 15:51:53 +0800

    feat: 🎸 add register api

--- server.js
+++ server.js
@@ -35,12 +35,26 @@ app.post('/signin', (req, res) => {
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
-  ){
-
+  ) {
     res.json('Successfully log in')
-  }else {
+  } else {
     res.status(404).json('error logging in')
   }
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body
+
+  database.users.push({
+    id: '123',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  })
+
+  res.json(database.users[database.users.length - 1])
 })
 
 app.listen(3000, () => console.log('app is running on port 3000'))
