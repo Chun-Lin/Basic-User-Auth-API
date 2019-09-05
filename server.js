92d3cd8efbf6146f5f841f6f3e9300679a912d02
Chun-Lin <wulin40063@gmail.com>
2019-09-05 11:41:14 +0800

    test: 💍 modify profile api by getting data from database

--- server.js
+++ server.js
@@ -50,17 +50,14 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      return res.json(user) // found it then break the loop and respond json
-    }
-  })
 
-  if (!found) {
-    res.status(400).json('not found')
-  }
+  db.select('*')
+    .from('users')
+    .where({ id: id })
+    .then(user =>
+      user.length > 0 ? res.json(user[0]) : res.status(400).json('Not Found'),
+    )
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
